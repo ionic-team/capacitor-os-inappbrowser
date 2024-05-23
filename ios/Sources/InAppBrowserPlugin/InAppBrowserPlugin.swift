c985813cc1cff3a002009bc7861fd9fe02b4bf97
OS-ricardomoreirasilva <ricardo.moreira.silva@outsystems.com>
2024-05-23 16:04:31 +0100

    feat(ios): add openInExternalBrowser
    This includes an adapter between OSIABRouter and UIApplication to perform the url open and a model containing the url to be consumed.
    Create a OpenInExternalBrowserParameterModel type with the method's parameters.
    Add the xcframework to the podspec file.
    
    References: https://outsystemsrd.atlassian.net/browse/RMET-3421

--- ios/Sources/InAppBrowserPlugin/InAppBrowserPlugin.swift
+++ ios/Sources/InAppBrowserPlugin/InAppBrowserPlugin.swift
@@ -1,5 +1,6 @@
-import Foundation
 import Capacitor
+import OSInAppBrowserLib
+import UIKit
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
@@ -10,14 +11,32 @@ public class InAppBrowserPlugin: CAPPlugin, CAPBridgedPlugin {
     public let identifier = "InAppBrowserPlugin"
     public let jsName = "InAppBrowser"
     public let pluginMethods: [CAPPluginMethod] = [
-        CAPPluginMethod(name: "echo", returnType: CAPPluginReturnPromise)
+        CAPPluginMethod(name: "openInExternalBrowser", returnType: CAPPluginReturnPromise)
     ]
-    private let implementation = InAppBrowser()
 
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
-        ])
+    private var plugin: OSIABEngine?
+
+    override public func load() {
+        self.plugin = .init(application: .shared)
+    }
+
+    @objc func openInExternalBrowser(_ call: CAPPluginCall) {
+        if self.plugin == nil { 
+            self.load() 
+        }
+
+        guard let plugin else {
+            return call.reject("Capacitor bridge is not initialized.")
+        }
+
+        guard let url = call.getString("url") else {
+            return call.reject("The input parameters for 'openInExternalBrowser' are invalid.")
+        }
+        
+        if plugin.openExternalBrowser(url) == true {
+            call.resolve()
+        } else {
+            call.reject("Couldn't open '\(url)' using Safari.")
+        }
     }
 }
