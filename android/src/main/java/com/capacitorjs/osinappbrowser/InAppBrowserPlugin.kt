b735b222e6caacd1ef2a0b213dab955a736f7246
Chace Daniels <chaced12@gmail.com>
2024-06-13 00:21:50 -0700

    feat(android): implement open with system browser / custom tabs

--- android/src/main/java/com/capacitorjs/osinappbrowser/InAppBrowserPlugin.kt
+++ android/src/main/java/com/capacitorjs/osinappbrowser/InAppBrowserPlugin.kt
@@ -4,8 +4,12 @@ import com.getcapacitor.PluginCall
 import com.getcapacitor.PluginMethod
 import com.getcapacitor.annotation.CapacitorPlugin
 import com.outsystems.plugins.inappbrowser.osinappbrowserlib.OSIABEngine
-import com.outsystems.plugins.inappbrowser.osinappbrowserlib.routeradapters.ApplicationContextAdapter
-import com.outsystems.plugins.inappbrowser.osinappbrowserlib.routeradapters.OSIABApplicationRouterAdapter
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.models.OSIABAnimation
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.models.OSIABBottomSheet
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.models.OSIABCustomTabsOptions
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.models.OSIABViewStyle
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.routeradapters.OSIABCustomTabsRouterAdapter
+import com.outsystems.plugins.inappbrowser.osinappbrowserlib.routeradapters.OSIABExternalBrowserRouterAdapter
 
 @CapacitorPlugin(name = "InAppBrowser")
 class InAppBrowserPlugin : Plugin() {
@@ -14,9 +18,9 @@ class InAppBrowserPlugin : Plugin() {
 
     override fun load() {
         super.load()
-        val applicationDelegate = ApplicationContextAdapter(context)
-        val router = OSIABApplicationRouterAdapter(applicationDelegate)
-        engine = OSIABEngine(router)
+        val externalBrowserRouter = OSIABExternalBrowserRouterAdapter(context)
+        val customTabsRouter = OSIABCustomTabsRouterAdapter(context)
+        engine = OSIABEngine(externalBrowserRouter, customTabsRouter)
     }
 
     @PluginMethod
@@ -28,6 +32,57 @@ class InAppBrowserPlugin : Plugin() {
         }
 
         engine?.openExternalBrowser(url) { success ->
+            if (success) {
+                call.resolve()
+            } else {
+                call.reject("Couldn't open '$url' using the external browser.")
+            }
+        }
+    }
+
+    @PluginMethod
+    fun openInSystemBrowser(call: PluginCall) {
+        val url = call.getString("url")
+        if (url == null) {
+            call.reject("The input parameters for 'openInExternalBrowser' are invalid.")
+            return
+        }
+
+        val optionsJson = call.getObject("options")?.getJSObject("android")
+
+        val options = optionsJson?.let {
+            val showTitle = it.getBoolean("showTitle", true) ?: true
+            val hideToolbarOnScroll = it.getBoolean("hideToolbarOnScroll", false) ?: false
+            val viewStyle = it.getInteger("viewStyle")?.let { ordinal ->
+                OSIABViewStyle.entries[ordinal]
+            } ?: OSIABViewStyle.FULL_SCREEN
+
+            val bottomSheetOptions = it.getJSObject("bottomSheetOptions")?.let { json ->
+                val height = json.getInteger("height", 1) ?: 1
+                val isFixed = json.getBoolean("isFixed", false) ?: false
+
+                OSIABBottomSheet(height, isFixed)
+            }
+
+            val startAnimation = it.getInteger("startAnimation")?.let { ordinal ->
+                OSIABAnimation.entries[ordinal]
+            } ?: OSIABAnimation.FADE_IN
+
+            val exitAnimation = it.getInteger("exitAnimation")?.let { ordinal ->
+                OSIABAnimation.entries[ordinal]
+            } ?: OSIABAnimation.FADE_OUT
+
+            OSIABCustomTabsOptions(
+                showTitle = showTitle,
+                hideToolbarOnScroll = hideToolbarOnScroll,
+                viewStyle = viewStyle,
+                bottomSheetOptions = bottomSheetOptions,
+                startAnimation = startAnimation,
+                exitAnimation = exitAnimation
+            )
+        }
+
+        engine?.openCustomTabs(url, options) { success ->
             if (success) {
                 call.resolve()
             } else {
