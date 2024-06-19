d7a039aaccfefde3a60d6179aa6d974d18c679f0
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2024-06-19 10:07:11 +0100

    refactor: simplify openInWebView method by using try-catch block
    References: https://outsystemsrd.atlassian.net/browse/RMET-3426

--- android/src/main/java/com/capacitorjs/osinappbrowser/InAppBrowserPlugin.kt
+++ android/src/main/java/com/capacitorjs/osinappbrowser/InAppBrowserPlugin.kt
@@ -41,67 +41,59 @@ class InAppBrowserPlugin : Plugin() {
 
     @PluginMethod
     fun openInWebView(call: PluginCall) {
-        val url = call.getString("url")
-        if (url.isNullOrEmpty()) {
-            call.reject("The input parameters for 'openInWebView' are invalid.")
-            return
-        }
+        try {
+            val url = call.getString("url")
+            val options = buildWebViewOptions(call.getObject("options"))
 
-        val options = buildWebViewOptions(call.getObject("options"))
-        if (options == null) {
-            call.reject("The input parameters for 'openInWebView' are invalid.")
-            return
-        }
-
-        engine?.openWebView(url, options) { success ->
-            if (success) {
-                call.resolve()
-            } else {
-                call.reject("Couldn't open '$url' using the web view.")
+            engine?.openWebView(url!!, options) { success ->
+                if (success) {
+                    call.resolve()
+                } else {
+                    call.reject("Couldn't open '$url' using the web view.")
+                }
             }
+        } catch (e: Exception) {
+            call.reject("The input parameters for 'openInWebView' are invalid.")
         }
+
     }
 
     /**
      * Parses options that come in a JSObject to create a 'OSIABWebViewOptions' object.
      * @param options The options to open the URL in a WebView.
      */
-    private fun buildWebViewOptions(options: JSObject?): OSIABWebViewOptions? {
-        return try {
-            options?.let {
-                val showURL = it.getBoolean("showURL", true) ?: true
-                val showToolbar = it.getBoolean("showToolbar", true) ?: true
-                val clearCache = it.getBoolean("clearCache", true) ?: true
-                val clearSessionCache = it.getBoolean("clearSessionCache", true) ?: true
-                val mediaPlaybackRequiresUserAction = it.getBoolean("mediaPlaybackRequiresUserAction", false) ?: false
-                val closeButtonText = it.getString("closeButtonText", "Close")
-                val toolbarPosition = it.getInteger("toolbarPosition")?.let { ordinal ->
-                    OSIABToolbarPosition.entries[ordinal]
-                } ?: OSIABToolbarPosition.TOP
-                val leftToRight = it.getBoolean("leftToRight", false) ?: false
-                val showNavigationButtons = it.getBoolean("showNavigationButtons", false) ?: false
-                val androidOptions = it.getJSObject("android")
-                val allowZoom = androidOptions?.getBoolean("allowZoom", true) ?: true
-                val hardwareBack = androidOptions?.getBoolean("hardwareBack", true) ?: true
-                val pauseMedia = androidOptions?.getBoolean("pauseMedia", true) ?: true
+    private fun buildWebViewOptions(options: JSObject): OSIABWebViewOptions {
+        return options.let {
+            val showURL = it.getBoolean("showURL", true) ?: true
+            val showToolbar = it.getBoolean("showToolbar", true) ?: true
+            val clearCache = it.getBoolean("clearCache", true) ?: true
+            val clearSessionCache = it.getBoolean("clearSessionCache", true) ?: true
+            val mediaPlaybackRequiresUserAction = it.getBoolean("mediaPlaybackRequiresUserAction", false) ?: false
+            val closeButtonText = it.getString("closeButtonText") ?: "Close"
+            val toolbarPosition = it.getInteger("toolbarPosition")?.let { ordinal ->
+                OSIABToolbarPosition.entries[ordinal]
+            } ?: OSIABToolbarPosition.TOP
+            val leftToRight = it.getBoolean("leftToRight", false) ?: false
+            val showNavigationButtons = it.getBoolean("showNavigationButtons", false) ?: false
+            val androidOptions = it.getJSObject("android")
+            val allowZoom = androidOptions?.getBoolean("allowZoom", true) ?: true
+            val hardwareBack = androidOptions?.getBoolean("hardwareBack", true) ?: true
+            val pauseMedia = androidOptions?.getBoolean("pauseMedia", true) ?: true
 
-                OSIABWebViewOptions(
-                    showURL,
-                    showToolbar,
-                    clearCache,
-                    clearSessionCache,
-                    mediaPlaybackRequiresUserAction,
-                    closeButtonText,
-                    toolbarPosition,
-                    leftToRight,
-                    showNavigationButtons,
-                    allowZoom,
-                    hardwareBack,
-                    pauseMedia
-                )
-            }
-        } catch (e: Exception) {
-            return null
+            OSIABWebViewOptions(
+                showURL,
+                showToolbar,
+                clearCache,
+                clearSessionCache,
+                mediaPlaybackRequiresUserAction,
+                closeButtonText,
+                toolbarPosition,
+                leftToRight,
+                showNavigationButtons,
+                allowZoom,
+                hardwareBack,
+                pauseMedia
+            )
         }
     }
 
