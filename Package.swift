0d4cc6c4d5e19c88310425bc2434fea329c6c8ba
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-09-08 11:31:10 +0100

    feat: Capacitor 8 support
    BREAKING CHANGE: Capacitor major version update requires major version update on the plugin.

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorInappbrowser",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v15)],
     products: [
         .library(
             name: "CapacitorInappbrowser",
