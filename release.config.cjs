86361b6701e9c5148f5910103e2157ebaaa93cfe
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-09-08 11:04:12 +0100

    feat: config cap 8

--- release.config.cjs
+++ release.config.cjs
@@ -8,26 +8,6 @@ module.exports = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
-    [
-      '@semantic-release/github',
-      {
-        successComment: false,
-        failComment: false,
-        releasedLabels: false,
-        addReleases: 'bottom',
-        releaseNotes: {
-          changelogFile: 'CHANGELOG.md'
-        }
-      }
-    ],
-    [
-      '@semantic-release/git',
-      {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }
-    ]
+    '@semantic-release/changelog'
   ]
 };
