e095c921006789e31dd03ab93b78959b631cfa26
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2025-10-28 11:58:55 +0000

    chore: remove next and dev branch from release config (not needed here)

--- release.config.cjs
+++ release.config.cjs
@@ -2,8 +2,6 @@ module.exports = {
   branches: [
     { name: 'main', channel: 'latest' },
     { name: '1.x-Maintenance', channel: 'latest-1', prerelease: false },
-    { name: 'next', channel: 'next', prerelease: true },
-    { name: 'dev', channel: 'dev', prerelease: true }
   ],
   repositoryUrl: 'https://github.com/ionic-team/capacitor-os-inappbrowser.git',
   plugins: [
