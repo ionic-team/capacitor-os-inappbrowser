e16d30db05f8439ece46243c85f2c07c6e252d27
Pedro Bilro <pedro.gustavo.bilro@outsystems.com>
2025-10-30 12:15:28 +0000

    ci: Update release for 1.x (#88)

--- release.config.cjs
+++ release.config.cjs
@@ -1,9 +1,7 @@
 module.exports = {
   branches: [
     { name: 'main', channel: 'latest' },
-    { name: '1.x-Maintenance', channel: 'latest' },
-    { name: 'next', channel: 'next', prerelease: true },
-    { name: 'dev', channel: 'dev', prerelease: true }
+    { name: '1.x-Maintenance', channel: 'latest-1', prerelease: false },
   ],
   repositoryUrl: 'https://github.com/ionic-team/capacitor-os-inappbrowser.git',
   plugins: [
