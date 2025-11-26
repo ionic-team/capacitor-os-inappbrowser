c255f9432685d33a109543270c64314d841c8b34
Chace Daniels <chaced12@gmail.com>
2025-11-26 09:16:14 -0600

    chore: update npm packages (#97)

--- release.config.cjs
+++ release.config.cjs
@@ -1,33 +1,34 @@
 module.exports = {
   branches: [
-    { name: 'main', channel: 'latest' },
-    { name: 'next', channel: 'next', prerelease: true },
-    { name: 'development', channel: 'dev', prerelease: true }
+    { name: "main", channel: "latest" },
+    { name: "next", channel: "next", prerelease: true },
+    { name: "development", channel: "dev", prerelease: true },
   ],
-  repositoryUrl: 'https://github.com/ionic-team/capacitor-os-inappbrowser.git',
+  repositoryUrl: "https://github.com/ionic-team/capacitor-os-inappbrowser.git",
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
-    '@semantic-release/npm',
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    "@semantic-release/changelog",
+    "@semantic-release/npm",
     [
-      '@semantic-release/github',
+      "@semantic-release/github",
       {
         successComment: false,
         failComment: false,
         releasedLabels: false,
-        addReleases: 'bottom',
+        addReleases: "bottom",
         releaseNotes: {
-          changelogFile: 'CHANGELOG.md'
-        }
-      }
+          changelogFile: "CHANGELOG.md",
+        },
+      },
     ],
     [
-      '@semantic-release/git',
+      "@semantic-release/git",
       {
-        assets: ['CHANGELOG.md', 'package.json'],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
-      }
-    ]
-  ]
+        assets: ["CHANGELOG.md", "package.json"],
+        message:
+          "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+      },
+    ],
+  ],
 };
