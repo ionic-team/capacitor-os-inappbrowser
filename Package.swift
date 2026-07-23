877f727f46cf497e097c935a80f823a27797d239
jcesarmobile <jcesarmobile@gmail.com>
2026-07-23 18:21:14 +0200

    feat!: Align plugin baselines with core (#113)
    Co-authored-by: Pedro Bilro <pedro.gustavo.bilro@outsystems.com>

--- Package.swift
+++ Package.swift
@@ -3,14 +3,14 @@ import PackageDescription
 
 let package = Package(
     name: "CapacitorInappbrowser",
-    platforms: [.iOS(.v15)],
+    platforms: [.iOS(.v16)],
     products: [
         .library(
             name: "CapacitorInappbrowser",
             targets: ["InAppBrowserPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "8.0.0"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "9.0.0-alpha.5"),
         .package(url: "https://github.com/OutSystems/OSInAppBrowserLib-iOS.git", exact: "2.3.2")
     ],
     targets: [
@@ -18,7 +18,6 @@ let package = Package(
             name: "InAppBrowserPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm"),
                 .product(name: "OSInAppBrowserLib", package: "OSInAppBrowserLib-iOS")
             ],
             path: "ios/Sources/InAppBrowserPlugin"),
