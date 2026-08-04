b210bc941a55a3d27d4a1bfa653934d7f3385790
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-08-04 14:17:22 +0100

    chore(example-apps): Update keyboard and haptics

--- example-app-spm/ios/App/CapApp-SPM/Package.swift
+++ example-app-spm/ios/App/CapApp-SPM/Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             targets: ["CapApp-SPM"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "9.0.0-alpha.5"),
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", exact: "9.0.0-alpha.6"),
         .package(name: "CapacitorApp", path: "../../../node_modules/@capacitor/app"),
         .package(name: "CapacitorHaptics", path: "../../../node_modules/@capacitor/haptics"),
         .package(name: "CapacitorInappbrowser", path: "../../../.."),
