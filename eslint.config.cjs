8d405f75d337adbb02665592ca539478a98a3c9e
Chace Daniels <chace.daniels@outsystems.com>
2026-09-16 10:27:33 -0500

    fix: remove dead eslint deps and refresh lockfiles for eslint 10

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,16 +1,16 @@
-const ionic = require('@ionic/eslint-config/recommended');
+const ionic = require("@ionic/eslint-config/recommended");
 
 module.exports = [
   {
     ignores: [
-      'node_modules/**',
-      'dist/**',
-      'build/**',
-      'example-app/**',
-      'example-app-spm/**',
-      '**/*.js',
-      '**/*.mjs',
-      '**/*.cjs',
+      "node_modules/**",
+      "dist/**",
+      "build/**",
+      "example-app/**",
+      "example-app-spm/**",
+      "**/*.js",
+      "**/*.mjs",
+      "**/*.cjs",
     ],
   },
   ...ionic,
